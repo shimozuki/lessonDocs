67b544757412ed79c013388be0c2580483323a56
Carlos Green <zonxa14@gmail.com>
2020-01-25 17:35:12 -0800

    ML

--- JS4/MachineLearning/index.js
+++ JS4/MachineLearning/index.js
@@ -1,8 +1,11 @@
+// index.js
 const brain = require('brain.js')
-const lib = require('./lib.js')
+const lib = require('./lib.js') // You will need to write this
 
-const trainingData = require('./provisionData.json')
+const trainingData = require('./provisionData.js')
 const makeArr = lib.makeTrainingData(trainingData)
 
+console.log(trainingData, 'TD')
+
 const model = new brain.NeuralNetwork()
-modle.tarin(makeArr)
+model.train(makeArr)
