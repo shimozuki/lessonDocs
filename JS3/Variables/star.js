7251be769f13da26ac8643e2346d02a5e281c027
Carlos Green <zonxa14@gmail.com>
2020-03-11 17:56:34 -0700

    nice

---
+++ JS3/Variables/star.js
@@ -0,0 +1,6 @@
+const star = { name: "Tarzan"}
+const star2 = star
+star2['friend'] = star
+star2['friend']['lover'] = "Jane"
+
+star['name'] = "Sarah"
--- JS3/Variables/student.js
+++ JS3/Variables/student.js
@@ -9,4 +9,7 @@ const firstName = student['first']
 const student2 = student
 student2['name'] = 'last'
 
+console.log(student2, 'Same as student but name: "last"')
+console.log(student, '{first: "Harry", ...}')
+
 student2[student2.name] = "Weasley"
